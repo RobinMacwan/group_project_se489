c9e1c390984e63cfb463babc0f7639ce23c24732
RobinMacwany <robin_macwan@yahoo.com>
2024-05-07 13:03:33 -0500

    param and looger added

--- se489_group_project/__init__.py
+++ se489_group_project/__init__.py
@@ -1,2 +1,22 @@
-from se489_group_project.models.model import MyNeuralNet
-from se489_group_project.predict_model import predict
+import os
+import sys
+import logging
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_dir = "logs"
+log_filepath = os.path.join(log_dir,"running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
+
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= logging_str,
+
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("grp_proj_Logger")
