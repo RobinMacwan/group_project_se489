a70c9a0387dad50264c13967e0b456a0f3a6635a
RobinMacwany <robin_macwan@yahoo.com>
2024-05-07 15:39:00 -0500

    adding working notebooks code

--- se489_group_project/config/configuration.py
+++ se489_group_project/config/configuration.py
@@ -75,7 +75,7 @@ class ConfigurationManager:
         eval_config = ModelEvaluationConfig(
             path_of_model="data/training/model.h5",
             training_data="data/raw/kidney-ct-scan-image",
-            mlflow_uri="https://dagshub.com/RobinMacwan/group_project.mlflow",
+            mlflow_uri="https://dagshub.com/RobinMacwan/group_project_se489.mlflow",
             all_params=self.params,
             params_image_size=self.params.IMAGE_SIZE,
             params_batch_size=self.params.BATCH_SIZE
