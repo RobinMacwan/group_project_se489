5b85e30a24e264464624a498f73ed6e73aeed27b
RobinMacwany <robin_macwan@yahoo.com>
2024-06-11 23:44:52 -0500

    adding CML again

---
+++ train_model_cml.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
+preds, target = [], []
+for batch in train_dataloader:
+    x, y = batch
+    probs = model(x)
+    preds.append(probs.argmax(dim=-1))
+    target.append(y.detach())
+target = torch.cat(target, dim=0)
+preds = torch.cat(preds, dim=0)
+report = classification_report(target, preds)
+with open("classification_report.txt", 'w') as outfile:
+    outfile.write(report)
+confmat = confusion_matrix(target, preds)
+disp = ConfusionMatrixDisplay(confusion_matrix = confmat)
+plt.savefig('confusion_matrix.png')
