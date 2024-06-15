35e4b63f4063de6ab399ff385eb028465da3240a
eTroupe5201 <eTroupe@depaul.edu>
2024-06-15 07:11:29 -0500

    Fixing mypy, fixing accidental deletion

--- se489_group_project/components/prepare_base_model.py
+++ se489_group_project/components/prepare_base_model.py
@@ -96,8 +96,8 @@ class PrepareBaseModel:
         full_model = tf.keras.models.Model(inputs=model.input, outputs=prediction)
 
         full_model.compile(
-            optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate),
-            loss=tf.keras.losses.CategoricalCrossentropy(),
+            optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate, momentum=0.9),
+            loss=tf.keras.losses.CategoricalCrossentropy(reduction="sum_over_batch_size"),
             metrics=["accuracy"],
         )
 
